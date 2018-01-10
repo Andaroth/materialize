fa52b649193fa690eed8498d747701f3adb3d786
Anda <ax.fiolle@gmail.com>
2018-01-10 21:22:31 +0100

    upd

--- js/app.js
+++ js/app.js
@@ -3,10 +3,11 @@ $(document).ready(function() {
   $('.parallax').parallax();
   // Carousel
   $('.carousel.carousel-slider').carousel({
-    'dist': 10,
-    'next': 2,
-    'duration': 2000,
-    'indicators': true
+    dist: 10,
+    next: 2,
+    duration: 2000,
+    indicators: true,
+    fullWidth: true
   });
   // Scrollspy
   $('.scrollspy').scrollSpy();
