3a851bab7378f1c009b300f8b8341d4128969f7c
Anda <ax.fiolle@gmail.com>
2018-01-11 09:41:34 +0100

    mail update

---
+++ js/mail.js
@@ -0,0 +1,22 @@
+$(document).ready(function(){
+  $("#name,#email,#select1,#textarea1").val("");
+  $(".forminput").keyup(function(){
+    var name = $("#name").val(),
+        email = $("#email").val(),
+        type = $("#select1").val(),
+        msg = $("#textarea1").val();
+    switch(type){
+      case "onepage": type="Site une page"; break;
+      case "multipage": type="Site multi-page"; break;
+      case "eshop": type="Boutique en ligne"; break;
+      case "pro": type="Site pro sur mesure"; break;
+      default: type="Message"; break;
+    }
+    $("#sendbtn").attr("href","mailto:ax.fiolle@gmail.com?subject="+type+" par "+name+" (mail: "+email+")&body="+msg);
+    if((name.length > 0) &&
+      (email.length > 0) &&
+      (msg.length > 0) ){
+      $("#sendbtn").removeAttr("disabled");
+    } else {$("#sendbtn").attr("disabled",true);}
+  });
+});
