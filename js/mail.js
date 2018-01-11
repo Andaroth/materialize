4d45bccc2032054e3e6fa99fdc183bfce714cf27
Anda <ax.fiolle@gmail.com>
2018-01-11 15:53:19 +0100

    upd mail

--- js/mail.js
+++ js/mail.js
@@ -1,20 +1,15 @@
 $(document).ready(function(){
   $("#select1").val("none");
-  $("#sendbtn").attr("disabled",true);
-  $("#sendbtn,#name,#email,#textarea1,.formlabel").addClass("hide");
+  $("#sendbtn,.formmail").attr("disabled",true);
   $("#select1").change(function() {
     var type = $("#select1").val();
-    if (type == "none") {
-      $("#sendbtn,#name,#email,#textarea1,.formlabel").addClass("hide");
-      $("#sendbtn,#name,#email,#textarea1").attr("disabled",true);
-    } else {
-      $("#sendbtn,#name,#email,#textarea1,.formlabel").removeClass("hide"); 
-      $("#name,#email,#textarea1").removeAttr("disabled");
-    } // else/if end
+    if (type == "none") {$("#sendbtn,#name,#email,#textarea1").attr("disabled",true);}
+    else {$("#name,#email,#textarea1").removeAttr("disabled");}
   }); // change end
   $(".forminput").keyup(function(){
     var name = $("#name").val(), email = $("#email").val(), type = $("#select1").val(), msg = $("#textarea1").val();
     switch(type){
+      case "none": break;
       case "onepage": type="Site une page"; break;
       case "multipage": type="Site multi-page"; break;
       case "eshop": type="Boutique en ligne"; break;
@@ -22,8 +17,8 @@ $(document).ready(function(){
       default: type="Message"; break;
     } // switch end
     $("#sendbtn").attr("href","mailto:ax.fiolle@gmail.com?subject=["+type+"] "+name+" (mail: "+email+")&body="+msg);
-    if((name.length > 0) && (email.length > 0) && (msg.length > 0) ){
-      $("#sendbtn").removeAttr("disabled");
-    } else {$("#sendbtn").attr("disabled",true);}
+    if((name.length > 0) && (email.length > 0) && (msg.length > 0) && (type != "none")){
+      $("#sendbtn,.formmail").removeAttr("disabled");
+    } else {$("#sendbtn,.formmail").attr("disabled",true);}
   }); // keyup end
 }); // ready end
